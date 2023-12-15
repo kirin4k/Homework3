c2a1ab36cc82e1aebe5502cafa88dbf54843ced6
Aibar Arpish <aibar201329@gmail.com>
2023-12-15 16:38:42 +0600

    Add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Dom1");
+int[] arr = new int[10];
+
+void FillArray(){
+    for(int i=0; i<arr.Length; i++){
+    arr[i] = new Random().Next(1,101);
+    Console.Write($"\t'{arr[i]}'");
+   }
+}
+FillArray();
+Console.WriteLine();
+int answer = FindArray(arr);
+Console.WriteLine($"Количество чисел в промежутке от 20 до 90: '{answer}'");
+
+
+int FindArray (int[] array){
+    int count =0;
+    for(int i =0; i< array.Length; i++){
+        if(array[i]>=20 && array[i]<=90){
+        count++;}
+    }
+    return count;
+}
+
+
