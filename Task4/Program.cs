50832da007b1257c557fa993b3d526f89262ff6f
Aibar Arpish <aibar201329@gmail.com>
2023-12-15 17:27:04 +0600

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Dom4");
+Console.Write("Введите число в диапазоне от 1 до 100 000: "); 
+string num = Console.ReadLine();
+int size = num.Length;
+int number = Convert.ToInt32(num);
+
+int[] array = new int[size];
+MakeAndPrintArray(array);
+
+void MakeAndPrintArray(int[] arr){
+for(int i=arr.Length-1; i>=0; i--){
+    arr[i] = number%10;
+    number= number/10;
+}
+for(int i = 0; i<arr.Length; i++){
+    Console.Write($"\t '{arr[i]}'");
+}
+}
